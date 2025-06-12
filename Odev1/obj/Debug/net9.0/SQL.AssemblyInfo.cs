8b7aaf4e14a1c04b166b7cf11f16c2d999aae30a
Nihat Cihan <nihat.cihan7@gmail.com>
2025-06-12 18:02:14 +0300

    6.soru eklendi

--- Odev1/obj/Debug/net9.0/SQL.AssemblyInfo.cs
+++ Odev1/obj/Debug/net9.0/SQL.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SQL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+807429e4f9091f282fe6bc0ff0050a3786c24413")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ec5d3e9ccd2b1ba9a1168185c0094f123f78ec4")]
 [assembly: System.Reflection.AssemblyProductAttribute("SQL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SQL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
